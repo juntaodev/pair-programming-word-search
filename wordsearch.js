a29ecb1f866a17be5ae448a4c40d36078e91b1eb
juntaodev <juntaoliudev@gmail.com>
2022-11-24 10:28:28 -0500

    word search function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  
+  let array = [];
+  for (let i =  0; i < matrix[0].length; i++) {
+    array.push([]);
+  }
 
-module.exports = wordSearch
+     
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      array[col][row]  = matrix[row][col];
+    }
+  }
+  // console.log(array);
+  return array;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    
+  let output = false;
+  let array = [];
+
+  for (let h of horizontalJoin) {
+    array.push(h);
+  }
+    
+  for (let v of verticalJoin) {
+    array.push(v);
+  }
+    
+  // console.log(array)
+
+  for (let el of array) {
+    if (el.includes(word)) {
+      output = true;
+    }
+  }
+
+  output ? true : false;
+
+  return output;
+};
+
+module.exports = wordSearch;
